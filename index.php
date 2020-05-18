c9478f37c5d126ce689fb3de23b9526fd4da002b
Dzubayyan Ahmad <masdzub@gmail.com>
2020-05-19 04:29:23 +0700

    add font size

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cek IP Public</title>
+</head>
+<body>
+<div style="font-size:60px;">
 <?php
     $ip = file_get_contents('https://api.ipify.org');
-    echo "IP anda adalah : " . $ip;
-
-    $ip6 = file_get_contents('https://api6.ipify.org	');
-    echo "IPv6 anda adalah: " . $ip6;
-
-    echo "jika IPv4 dan IPv6 anda sama, maka IPv6 anda tidak ada atau jaringan anda belum memiliki IPv6"
+    echo "IP anda adalah : " . $ip; 
+    echo "<br>";
+    $ip6 = file_get_contents('https://api6.ipify.org');
+    echo "IPv6 anda adalah: " . $ip6; 
+ ?>
+ </div>
+ <div>
+ <?php
+    echo "<br>";
+    echo "jika IPv4 dan IPv6 anda sama, maka IPv6 anda tidak ada atau jaringan anda belum memiliki IPv6" 
 ?>
+</div>
+</body>
+</html>
