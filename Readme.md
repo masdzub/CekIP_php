b08574d7f149654eb4c24e3aeff885f00685059e
Dzubayyan Ahmad <masdzub@gmail.com>
2020-05-19 04:39:07 +0700

    sasasa

--- Readme.md
+++ Readme.md
@@ -1,1 +1,6 @@
-#Cek IP PHP
+#Cek IP di server / local
+
+Pastikan dijalankan dari koneksi yang ingin di check.
+Semisal di jalankan di server. Maka IP yang akan terbaca adalah IP server
+
+Jika di jalankan di local maka yang terbaca adalah IP local atau jaringan anda
--- index.php
+++ index.php
@@ -6,17 +6,17 @@
 <body>
 <div style="font-size:60px;">
 <?php
-    $ip = file_get_contents('https://api.ipify.org');
-    echo "IP anda adalah : " . $ip; 
-    echo "<br>";
+    $ip4 = file_get_contents('https://api.ipify.org');
+    echo "IPv4 anda adalah : " . $ip4; 
+    echo "<br><br>";
     $ip6 = file_get_contents('https://api6.ipify.org');
     echo "IPv6 anda adalah: " . $ip6; 
  ?>
  </div>
- <div>
+ <div style="font-size: 30px;">
  <?php
     echo "<br>";
-    echo "jika IPv4 dan IPv6 anda sama, maka IPv6 anda tidak ada atau jaringan anda belum memiliki IPv6" 
+    echo "Note : Jika IPv4 dan IPv6 anda sama, maka IPv6 anda tidak ada atau jaringan anda belum memiliki IPv6" 
 ?>
 </div>
 </body>
